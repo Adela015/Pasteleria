90ac6de2b827748a04d640243ec1132cf6d31b62
Adela015 <ade.cervantes01@gmail.com>
2025-09-30 03:45:41 -0300

    Cards de categorias y Carrousel placeholder

---
+++ assets/js/navbar.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
